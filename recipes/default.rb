f6c570fcb6b3930463a2c569326c47df1172b765
Bill Meyer <bill@chef.io>
2017-08-21 16:34:52 -0500

    Initial commits

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'epel-release'
+
+file '/bin/chef-client' do
+  action :delete
+end
+
+file '/usr/bin/chef-client' do
+  action :delete
+end
+
+cookbook_file '/usr/bin/chef-client' do
+  source 'chef-client'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
